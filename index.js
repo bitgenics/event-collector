1f60f64ea468ee722ce6ff878cb2bc9ef588879f
Sander Huijsen <sander.huijsen@gmail.com>
2017-07-25 10:56:43 +0800

    Initialise meta. Fixed bug re. duplicateIds. Some code style changes.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const toMs = (end, start) => {
-	const ms = (1000 * (end[0] - start[0])) + ((end[1] - start[1])/1000000);
-	return Math.round(ms*1000)/1000;
-}
+	const ms = (1000 * (end[0] - start[0])) + ((end[1] - start[1]) / 1000000);
+	return Math.round(ms * 1000) / 1000;
+};
 
 class EventCollector {
 	constructor(meta) {
-		this.meta = meta;
+		this.meta = meta || {};
 		this.time_epoch = Date.now();
 		this.time_date = new Date();
 		this.start_hr = process.hrtime();
@@ -25,8 +25,8 @@ class EventCollector {
 	}
 
 	_getId(type) {
-		if(this.jobs[type]) {
-			const counter = duplicateIds[type] ? 1 : duplicateIds[type]++;
+		if (this.jobs[type]) {
+			const counter = this.duplicateIds[type] ? 1 : this.duplicateIds[type]++;
 			return `${type}-${counter}`;
 		} else {
 			return type;
@@ -35,10 +35,10 @@ class EventCollector {
 
 	startJob(type, meta) {
 		const start_hr = process.hrtime();
-		const job = {start_hr, startAtMs: toMs(start_hr, this.start_hr)};
+		const job = { start_hr, startAtMs: toMs(start_hr, this.start_hr) };
 		Object.assign(job, meta);
 		const id = this._getId(type);
-		this.jobs[id] = job
+		this.jobs[id] = job;
 		return id;
 	}
 
