b6174baea325dcca2bb2d05ccdcac75936391c6f
Erwin van der Koogh <erwin@bitgenics.io>
2017-08-31 10:40:11 +1000

    Fix formatting

--- express-eventcollector.js
+++ express-eventcollector.js
@@ -12,18 +12,18 @@ const wrapper = (EventCollector) => {
       req.eventcollector.addMeta(meta);
       res.on('close', () => {
         const res_meta = {
-            res_statusCode: res.statusCode,
-            res_conn_status: 'connection terminated'
+          res_statusCode: res.statusCode,
+          res_conn_status: 'connection terminated'
         };
         req.eventcollector.end(res_meta);
         onEnd(req.eventcollector);
       });
       res.on('finish', () => {
         const res_meta = {
-            res_statusCode: res.statusCode,
-            res_contentType: res.getHeader('content-type'),
-            res_contentEncoding: res.getHeader('content-encoding'),
-            res_cacheHeader: res.getHeader('cache-control')
+          res_statusCode: res.statusCode,
+          res_contentType: res.getHeader('content-type'),
+          res_contentEncoding: res.getHeader('content-encoding'),
+          res_cacheHeader: res.getHeader('cache-control')
         };
         req.eventcollector.end(res_meta);
         onEnd(req.eventcollector);
--- index.js
+++ index.js
@@ -1,118 +1,118 @@
 const { Writable } = require('stream');
 
 const toMs = (end, start) => {
-	if(!(end && start)) { return -1 }
-	const ms = (1000 * (end[0] - start[0])) + ((end[1] - start[1]) / 1000000);
-	return Math.round(ms * 1000) / 1000;
+    if(!(end && start)) { return -1 }
+    const ms = (1000 * (end[0] - start[0])) + ((end[1] - start[1]) / 1000000);
+    return Math.round(ms * 1000) / 1000;
 };
 
 class LogWritable extends Writable {
-	constructor(eventcollector, logLevel) {
-		super({
-			decodeStrings: false,
-			objectMode: true
-		});
-		this.collector = eventcollector;
-		this.logLevel = logLevel;
-	}
+    constructor(eventcollector, logLevel) {
+        super({
+            decodeStrings: false,
+            objectMode: true
+        });
+        this.collector = eventcollector;
+        this.logLevel = logLevel;
+    }
 
-	_write(chunk, encoding, callback) {
-		this.collector.log(this.logLevel, chunk);
-		callback();
-	}
+    _write(chunk, encoding, callback) {
+        this.collector.log(this.logLevel, chunk);
+        callback();
+    }
 }
 
 class EventCollector {
-	constructor(meta) {
-		this.event = {
-			meta: meta || {},
-			time_epoch: Date.now(),
-			time_date: new Date(),
-			jobs: {},
-			errors: [],
-			errorCount: 0
-		};
-		this.open_jobs = new Set(),
-		this.start_hr = process.hrtime();
-		this.duplicateIds = {};
-		this.logs = [];
-	}
+    constructor(meta) {
+        this.event = {
+            meta: meta || {},
+            time_epoch: Date.now(),
+            time_date: new Date(),
+            jobs: {},
+            errors: [],
+            errorCount: 0
+        };
+        this.open_jobs = new Set(),
+        this.start_hr = process.hrtime();
+        this.duplicateIds = {};
+        this.logs = [];
+    }
 
-	addMeta(meta) {
-		Object.assign(this.event.meta, meta);
-	}
+    addMeta(meta) {
+        Object.assign(this.event.meta, meta);
+    }
 
-	addError(error) {
-		const message = error.message || error;
-		const time = toMs(process.hrtime(), this.start_hr);
-		const err = { message, time, open_jobs: Array.from(this.open_jobs) };
-		if(error.stack) { err.stack = error.stack };
-		this.event.errors.push(err);
-		this.event.errorCount++
-	}
+    addError(error) {
+        const message = error.message || error;
+        const time = toMs(process.hrtime(), this.start_hr);
+        const err = { message, time, open_jobs: Array.from(this.open_jobs) };
+        if(error.stack) { err.stack = error.stack };
+        this.event.errors.push(err);
+        this.event.errorCount++
+    }
 
-	_getId(type) {
-		if(this.event.jobs[type]) {
-		    // Initialise or update duplicate id count for 'type'
-		    this.duplicateIds[type] = !this.duplicateIds[type] ? 1 : this.duplicateIds[type] + 1;
-			return `${type}-${this.duplicateIds[type]}`;
-		} else {
-			return type;
-		}
-	}
+    _getId(type) {
+        if(this.event.jobs[type]) {
+            // Initialise or update duplicate id count for 'type'
+            this.duplicateIds[type] = !this.duplicateIds[type] ? 1 : this.duplicateIds[type] + 1;
+            return `${type}-${this.duplicateIds[type]}`;
+        } else {
+            return type;
+        }
+    }
 
-	startJob(type, meta) {
-		const start_hr = process.hrtime();
-		const job = { start_hr, startAtMs: toMs(start_hr, this.start_hr) };
-		Object.assign(job, meta);
-		const id = this._getId(type);
-		this.event.jobs[id] = job;
-		this.open_jobs.add(id);
-		return id;
-	}
+    startJob(type, meta) {
+        const start_hr = process.hrtime();
+        const job = { start_hr, startAtMs: toMs(start_hr, this.start_hr) };
+        Object.assign(job, meta);
+        const id = this._getId(type);
+        this.event.jobs[id] = job;
+        this.open_jobs.add(id);
+        return id;
+    }
 
-	endJob(id, meta) {
-		const job = this.event.jobs[id];
-		if (job) {
+    endJob(id, meta) {
+        const job = this.event.jobs[id];
+        if (job) {
             Object.assign(job, meta);
             this.open_jobs.delete(id);
-			if (job.start_hr) {
+            if (job.start_hr) {
                 job.durationInMs = toMs(process.hrtime(), job.start_hr);
                 delete job.start_hr;
-			} else {
-				this.addError(`EventCollector - Job: '${id}' was already ended`);
-			}
-		} else {
-			this.addError(`EventCollector - Job: '${id}' was not started`);
-		}
-	}
+            } else {
+                this.addError(`EventCollector - Job: '${id}' was already ended`);
+            }
+        } else {
+            this.addError(`EventCollector - Job: '${id}' was not started`);
+        }
+    }
 
-	logStdOut(){
-		return new LogWritable(this, 'LOG');
-	}
+    logStdOut(){
+        return new LogWritable(this, 'LOG');
+    }
 
-	logStdErr(){
-		return new LogWritable(this, 'ERR');
-	}
+    logStdErr(){
+        return new LogWritable(this, 'ERR');
+    }
 
-	log(logLevel, log) {
-		const atMs = toMs(process.hrtime(), this.start_hr);
-		this.logs.push({logLevel, atMs, log});
-	}
+    log(logLevel, log) {
+        const atMs = toMs(process.hrtime(), this.start_hr);
+        this.logs.push({logLevel, atMs, log});
+    }
 
-	getEvent() {
-		return this.event;
-	}
+    getEvent() {
+        return this.event;
+    }
 
-	getLogs() {
-		return this.logs;
-	}
+    getLogs() {
+        return this.logs;
+    }
 
-	end(meta) {
-		this.open_jobs.forEach((id) => { this.endJob(id) });
-		this.event.totalDuration = toMs(process.hrtime(), this.start_hr);
-		Object.assign(this.event.meta, meta);
-	}
+    end(meta) {
+        this.open_jobs.forEach((id) => { this.endJob(id) });
+        this.event.totalDuration = toMs(process.hrtime(), this.start_hr);
+        Object.assign(this.event.meta, meta);
+    }
 }
 
 EventCollector.express = require('./express-eventcollector')(EventCollector);
